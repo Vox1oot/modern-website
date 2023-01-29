846b4ffefc2c1b0effd2f7ae6e3f635f6b120c3b
morsa <voxfoot@gmail.com>
2023-01-30 00:14:45 +0500

    add icons for business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styles, { layout } from '../styles';
 import Button from './Button';
+import { shield, send, star } from '../assets';
+
+const mapping = {
+    shield,
+    send,
+    star,
+};
+
+const FeatureCard = ({
+    icon, title, content, index,
+}) => (
+    <div>
+        <div>
+            <img src={mapping[icon]} alt={icon} />
+        </div>
+    </div>
+);
 
 const Business = () => {
     const { t } = useTranslation();
@@ -13,6 +30,12 @@ const Business = () => {
                 <h2 className={styles.heading2}>{`${t('features_h2')}`}</h2>
                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>{`${t('features_p')}`}</p>
                 <Button styles="mt-10" />
+            </div>
+
+            <div className={`${layout.sectionImg} flex-col`}>
+                {features.map(({ id, ...rest }, index) => (
+                    <FeatureCard key={id} {...rest} index={index} />
+                ))}
             </div>
         </section>
     );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 const Button = ({ styles }) => {
     const { t } = useTranslation();
+
     return (
         <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>
             {`${t('getStarted.get')} ${t('getStarted.started')}`}

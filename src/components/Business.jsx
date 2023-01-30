2adef611f69344f65b1562b2e59e0f1d943d2f48
morsa <voxfoot@gmail.com>
2023-01-30 23:51:09 +0500

    implment Business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -11,18 +11,31 @@ const mapping = {
 };
 
 const FeatureCard = ({
-    icon, title, content, index,
+    icon, title, content, index, len,
 }) => (
-    <div>
-        <div>
-            <img src={mapping[icon]} alt={icon} />
+    <div className={`flex flex-col xs:flex-row p-6 rounded-[20px] 
+        ${index !== (len - 1) ? 'mb-6' : 'mb-0'} feature-card items-center`}
+    >
+        <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue mb-5 xs:mb-0`}>
+            <img
+                src={mapping[icon]}
+                alt={icon}
+                className="w-[50%] h-[50%] object-contain"
+            />
         </div>
+
+        <div className="flex-1 flex flex-col ml-3 cursor-pointer">
+            <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+            <p className={`${styles.paragraph}`}>{content}</p>
+        </div>
+
     </div>
 );
 
 const Business = () => {
     const { t } = useTranslation();
     const features = t('features', { returnObjects: true });
+    const len = features.lenght;
 
     return (
         <section id="features" className={layout.section}>
@@ -33,8 +46,17 @@ const Business = () => {
             </div>
 
             <div className={`${layout.sectionImg} flex-col`}>
-                {features.map(({ id, ...rest }, index) => (
-                    <FeatureCard key={id} {...rest} index={index} />
+                {features.map(({
+                    id, icon, title, content,
+                }, index) => (
+                    <FeatureCard
+                        key={id}
+                        icon={icon}
+                        title={title}
+                        content={content}
+                        index={index}
+                        len={len}
+                    />
                 ))}
             </div>
         </section>

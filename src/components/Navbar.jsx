4bb005e343bc8a5d10c904affd5baf8f99199cd7
Vitaliy Morzhov <voxfoot@gmail.com>
2023-02-12 17:36:41 +0500

    chore: add styles for btn language

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         const nextLanguage = language === 'en' ? 'ru' : 'en';
         i18n.changeLanguage(nextLanguage);
         setLanguage(nextLanguage);
-    }
+    };
 
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -24,11 +24,12 @@ const Navbar = () => {
                 <li className="font-poppins cursor-pointer text-[18px] text-white mr-10"><a href="#features">{t('navLinks.features')}</a></li>
                 <li className="font-poppins cursor-pointer text-[18px] text-white mr-10"><a href="#product">{t('navLinks.product')}</a></li>
                 <li className="font-poppins cursor-pointer text-[18px] text-white mr-10"><a href="#clients">{t('navLinks.clients')}</a></li>
-                <button 
-                    className='text-white'
+                <button
+                    className=" w-[40px] h-[40px] text-white text-[18px] border-2 rounded-full p-3 flex items-center justify-center"
+                    type="button"
                     onClick={toggleLanguage}
                 >
-                    {language}
+                    <span>{language}</span>
                 </button>
             </ul>
 
@@ -46,7 +47,14 @@ const Navbar = () => {
                         <li className="font-poppins cursor-pointer text-[14px] text-white mb-4"><a href="#home">{t('navLinks.home')}</a></li>
                         <li className="font-poppins cursor-pointer text-[14px] text-white mb-4"><a href="#features">{t('navLinks.features')}</a></li>
                         <li className="font-poppins cursor-pointer text-[14px] text-white mb-4"><a href="#product">{t('navLinks.product')}</a></li>
-                        <li className="font-poppins cursor-pointer text-[14px] text-white"><a href="#clients">{t('navLinks.clients')}</a></li>
+                        <li className="font-poppins cursor-pointer text-[14px] text-white mb-4"><a href="#clients">{t('navLinks.clients')}</a></li>
+                        <button
+                            className="w-[100%] h-[2rem] text-white text-[16px] border-2 p-2 rounded-[10px] flex items-center justify-center"
+                            onClick={toggleLanguage}
+                            type="button"
+                        >
+                            <span>{language}</span>
+                        </button>
                     </ul>
                 </div>
             </div>
